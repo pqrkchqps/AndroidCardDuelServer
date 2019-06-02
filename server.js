60e81925b825745455b83f0dc23ff3607de68bf8
John A Creps <johnacreps@gmail.com>
2019-06-01 22:11:22 -0500

    changed interval again

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ io.on('connection', function(socket) {
 
    var myInterval = setInterval(function () {
      socket.emit("ping", "ping");
-   }, 5000);
+   }, 3000);
 
    //Whenever someone disconnects this piece of code executed
    socket.on('disconnect', function () {
